b7ff621b39c89111a92f22e38312f6e81c8baeaf
chadwick <chadwickkoo@outlook.com>
2024-03-22 07:46:47 +0300

    modifies index.js file

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+function multiply(num1,num2){
+    
+	return num1*num2;
+}
+let num1=31;
+let num2=2;
+multiply(num1,num2);
 
-// Write your code here
+function random(){
+    return Math.random()
+}
+random()
+
+function mod(){
+    return num3%num4
+}
+let num3=14;
+let num4=5;
+mod()
+
+function max(){
+    return Math.max(0,5,10,15,20)
+}
+max()
